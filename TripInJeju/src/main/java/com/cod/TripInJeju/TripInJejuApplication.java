97dd377b49b6b34b7d88e39fc619b8b8b0765425
PILSUNyou <dbyo1125@naver.com>
2024-07-14 21:16:59 +0900

    first commit and write a README

---
+++ TripInJeju/src/main/java/com/cod/TripInJeju/TripInJejuApplication.java
@@ -0,0 +1,13 @@
+package com.cod.TripInJeju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripInJejuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripInJejuApplication.class, args);
+	}
+
+}
